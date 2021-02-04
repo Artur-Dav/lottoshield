e9cbce4bc58c20c77de2b634e0fb53b4fc0b9164
Artur <arturdavtyanars@gmail.com>
2021-02-04 15:49:14 +0400

    s

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 const mobileMenu = document.querySelector('.mobile-nav-bar')
 const menuBurger = document.querySelector('#burger')
+const desktopSliderSecondaryBlocks = document.querySelectorAll('.secondary-block')
+const desktopSliderPrimaryBlock = document.querySelector('.primary-block')
 
 // Functions
 const toggleMenu = () => {
@@ -11,4 +13,15 @@ menuBurger.addEventListener('click', toggleMenu)
 
 document.addEventListener('click', e => {
   e.target.classList.contains('mobile-nav-bar__menus--link') && toggleMenu()
+})
+
+desktopSliderSecondaryBlocks.forEach(el => {
+  el.addEventListener('mouseenter', e => {
+    el.style.transform = 'scale(1.35)'
+    desktopSliderPrimaryBlock.style.transform = 'scale(0.75)'
+  })
+  el.addEventListener('mouseleave', e => {
+    el.style.transform = 'scale(1)'
+    desktopSliderPrimaryBlock.style.transform = 'scale(1)'
+  })
 })
