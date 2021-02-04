1395f6bfb233d0b1edbe9a48b507d1dfd83038f0
Artur <arturdavtyanars@gmail.com>
2021-02-04 17:09:56 +0400

    s]

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,24 @@
-console.log(1)
+const primaryBlock = document.querySelector('.primary-block')
+
 // Functions
-// const toggleMenu = () => {
-//   document.querySelector('.mobile-nav-bar').classList.toggle('open')
-// }
+const toggleMenu = () => {
+  document.querySelector('.mobile-nav-bar').classList.toggle('open')
+}
 
 // Event listeners
-// document.querySelector('#burger').addEventListener('click', toggleMenu)
-document.querySelector('#burger').addEventListener('click', () => document.querySelector('.mobile-nav-bar').classList.toggle('open'))
+document.querySelector('#burger').addEventListener('click', toggleMenu)
 
 document.addEventListener('click', e => {
-  e.target.classList.contains('mobile-nav-bar__menus--link') && document.querySelector('.mobile-nav-bar').classList.toggle('open')
+  e.target.classList.contains('mobile-nav-bar__menus--link') && toggleMenu()
 })
 
 document.querySelectorAll('.secondary-block').forEach(el => {
   el.addEventListener('mouseenter', e => {
     el.style.transform = 'scale(1.35)'
-    document.querySelector('.primary-block').style.transform = 'scale(0.75)'
+    primaryBlock.style.transform = 'scale(0.75)'
   })
   el.addEventListener('mouseleave', e => {
     el.style.transform = 'scale(1)'
-    document.querySelector('.primary-block').style.transform = 'scale(1)'
+    primaryBlock.style.transform = 'scale(1)'
   })
 })
